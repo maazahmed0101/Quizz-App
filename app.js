9e2b587c6d2fb344e1b66764ff3e9d89c92488c3
Maaz-Ahmed <wwwmaazahmed1@gmail.com>
2026-03-30 08:25:20 +0500

    update

---
+++ app.js
@@ -0,0 +1,74 @@
+const questions = [
+    {
+        question: "What does HTML stand for?",
+        option1: "Hyper Text Markup Language",
+        option2: "Home Tool Markup Language",
+        option3: "Hyperlinks and Text Markup Language",
+        correctAnswer: "Hyper Text Markup Language",
+    },
+    {
+        question: "Which HTML element is used for the largest heading?",
+        option1: "<h6>",
+        option2: "<h1>",
+        option3: "<header>",
+        correctAnswer: "<h1>",
+    },
+    {
+        question: "What is the correct HTML element for inserting a line break?",
+        option1: "<br>",
+        option2: "<lb>",
+        option3: "<break>",
+        correctAnswer: "<br>",
+    },
+    {
+        question:
+            "Which attribute is used to provide an alternative text for an image?",
+        option1: "alt",
+        option2: "title",
+        option3: "src",
+        correctAnswer: "alt",
+    },
+    {
+        question: "What is the correct way to make a checkbox in HTML?",
+        option1: "<input type='checkbox'>",
+        option2: "<checkbox>",
+        option3: "<input type='box'>",
+        correctAnswer: "<input type='checkbox'>",
+    },
+    {
+        question: "Which HTML tag is used to create an unordered list?",
+        option1: "<ul>",
+        option2: "<ol>",
+        option3: "<list>",
+        correctAnswer: "<ul>",
+    },
+    {
+        question: "What does the <title> element define?",
+        option1: "The document's body",
+        option2: "The document's title in the browser toolbar",
+        option3: "The main heading",
+        correctAnswer: "The document's title in the browser toolbar",
+    },
+    {
+        question: "Which tag is used to define a hyperlink in HTML?",
+        option1: "<link>",
+        option2: "<href>",
+        option3: "<a>",
+        correctAnswer: "<a>",
+    },
+    {
+        question:
+            "What is the correct syntax for adding a background color in HTML?",
+        option1: "<body bg='color'>",
+        option2: "<body style='background-color: color;'>",
+        option3: "<background color='color'>",
+        correctAnswer: "<body style='background-color: color;'>",
+    },
+    {
+        question: "How can you make text bold in HTML?",
+        option1: "<strong>",
+        option2: "<b>",
+        option3: "Both <strong> and <b>",
+        correctAnswer: "Both <strong> and <b>",
+    },
+];
